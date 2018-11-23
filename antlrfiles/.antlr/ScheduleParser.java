a9ece2a8677ece0a9475471e29c487202ce84b25
Diki Ardian <ardiandiki369@gmail.com>
2018-11-23 20:17:02 +0700

    fix parser

--- antlrfiles/.antlr/ScheduleParser.java
+++ antlrfiles/.antlr/ScheduleParser.java
@@ -16,9 +16,10 @@ public class ScheduleParser extends Parser {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, T__6=7, T__7=8, BasicOperator=9, 
-		AssignmentOperator=10, OtherOperator=11, Kind=12, Name=13, Class=14, Dosen=15, 
-		Capacity=16, Facility=17, Sks=18, Tingkat=19, Day=20, Time=21, WhiteSpace=22;
+		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, T__6=7, T__7=8, T__8=9, 
+		T__9=10, T__10=11, T__11=12, BasicOperator=13, AssignmentOperator=14, 
+		OtherOperator=15, Kind=16, Name=17, Class=18, Dosen=19, Capacity=20, Facility=21, 
+		Sks=22, Tingkat=23, Day=24, Time=25, WhiteSpace=26;
 	public static final int
 		RULE_model = 0, RULE_assign = 1, RULE_end = 2;
 	public static final String[] ruleNames = {
@@ -27,12 +28,13 @@ public class ScheduleParser extends Parser {
 
 	private static final String[] _LITERAL_NAMES = {
 		null, "'('", "'kapasitas'", "':'", "','", "'fasilitas'", "'sks'", "'tingkat'", 
-		"')'"
+		"')'", "'kelas'", "'dosen'", "'hari'", "'jam'"
 	};
 	private static final String[] _SYMBOLIC_NAMES = {
-		null, null, null, null, null, null, null, null, null, "BasicOperator", 
-		"AssignmentOperator", "OtherOperator", "Kind", "Name", "Class", "Dosen", 
-		"Capacity", "Facility", "Sks", "Tingkat", "Day", "Time", "WhiteSpace"
+		null, null, null, null, null, null, null, null, null, null, null, null, 
+		null, "BasicOperator", "AssignmentOperator", "OtherOperator", "Kind", 
+		"Name", "Class", "Dosen", "Capacity", "Facility", "Sks", "Tingkat", "Day", 
+		"Time", "WhiteSpace"
 	};
 	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
 
@@ -253,6 +255,8 @@ public class ScheduleParser extends Parser {
 		public TerminalNode AssignmentOperator() { return getToken(ScheduleParser.AssignmentOperator, 0); }
 		public TerminalNode Class() { return getToken(ScheduleParser.Class, 0); }
 		public TerminalNode Dosen() { return getToken(ScheduleParser.Dosen, 0); }
+		public TerminalNode Day() { return getToken(ScheduleParser.Day, 0); }
+		public TerminalNode Time() { return getToken(ScheduleParser.Time, 0); }
 		public List<TerminalNode> Name() { return getTokens(ScheduleParser.Name); }
 		public TerminalNode Name(int i) {
 			return getToken(ScheduleParser.Name, i);
@@ -290,12 +294,36 @@ public class ScheduleParser extends Parser {
 			setState(62);
 			match(T__0);
 			setState(63);
-			match(Class);
+			match(T__8);
 			setState(64);
-			match(T__3);
+			match(T__2);
 			setState(65);
-			match(Dosen);
+			match(Class);
 			setState(66);
+			match(T__3);
+			setState(67);
+			match(T__9);
+			setState(68);
+			match(T__2);
+			setState(69);
+			match(Dosen);
+			setState(70);
+			match(T__3);
+			setState(71);
+			match(T__10);
+			setState(72);
+			match(T__2);
+			setState(73);
+			match(Day);
+			setState(74);
+			match(T__3);
+			setState(75);
+			match(T__11);
+			setState(76);
+			match(T__2);
+			setState(77);
+			match(Time);
+			setState(78);
 			match(T__7);
 			}
 			}
@@ -325,7 +353,7 @@ public class ScheduleParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(68);
+			setState(80);
 			match(OtherOperator);
 			}
 		}
@@ -341,25 +369,28 @@ public class ScheduleParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\30I\4\2\t\2\4\3\t"+
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\34U\4\2\t\2\4\3\t"+
 		"\3\4\4\t\4\3\2\3\2\3\2\7\2\f\n\2\f\2\16\2\17\13\2\3\2\3\2\3\2\3\2\7\2"+
 		"\25\n\2\f\2\16\2\30\13\2\3\2\3\2\3\2\3\2\7\2\36\n\2\f\2\16\2!\13\2\3\2"+
 		"\3\2\3\2\3\2\7\2\'\n\2\f\2\16\2*\13\2\3\2\3\2\3\2\3\2\7\2\60\n\2\f\2\16"+
 		"\2\63\13\2\3\2\7\2\66\n\2\f\2\16\29\13\2\3\3\3\3\6\3=\n\3\r\3\16\3>\3"+
-		"\3\3\3\3\3\3\3\3\3\3\3\3\4\3\4\3\4\2\2\5\2\4\6\2\2\2L\2\b\3\2\2\2\4:\3"+
-		"\2\2\2\6F\3\2\2\2\b\t\7\13\2\2\t\r\7\16\2\2\n\f\7\17\2\2\13\n\3\2\2\2"+
-		"\f\17\3\2\2\2\r\13\3\2\2\2\r\16\3\2\2\2\16\67\3\2\2\2\17\r\3\2\2\2\20"+
-		"\26\7\3\2\2\21\22\7\4\2\2\22\23\7\5\2\2\23\25\7\22\2\2\24\21\3\2\2\2\25"+
-		"\30\3\2\2\2\26\24\3\2\2\2\26\27\3\2\2\2\27\37\3\2\2\2\30\26\3\2\2\2\31"+
-		"\32\7\6\2\2\32\33\7\7\2\2\33\34\7\5\2\2\34\36\7\23\2\2\35\31\3\2\2\2\36"+
-		"!\3\2\2\2\37\35\3\2\2\2\37 \3\2\2\2 (\3\2\2\2!\37\3\2\2\2\"#\7\6\2\2#"+
-		"$\7\b\2\2$%\7\5\2\2%\'\7\24\2\2&\"\3\2\2\2\'*\3\2\2\2(&\3\2\2\2()\3\2"+
-		"\2\2)\61\3\2\2\2*(\3\2\2\2+,\7\6\2\2,-\7\t\2\2-.\7\5\2\2.\60\7\25\2\2"+
-		"/+\3\2\2\2\60\63\3\2\2\2\61/\3\2\2\2\61\62\3\2\2\2\62\64\3\2\2\2\63\61"+
-		"\3\2\2\2\64\66\7\n\2\2\65\20\3\2\2\2\669\3\2\2\2\67\65\3\2\2\2\678\3\2"+
-		"\2\28\3\3\2\2\29\67\3\2\2\2:<\7\f\2\2;=\7\17\2\2<;\3\2\2\2=>\3\2\2\2>"+
-		"<\3\2\2\2>?\3\2\2\2?@\3\2\2\2@A\7\3\2\2AB\7\20\2\2BC\7\6\2\2CD\7\21\2"+
-		"\2DE\7\n\2\2E\5\3\2\2\2FG\7\r\2\2G\7\3\2\2\2\t\r\26\37(\61\67>";
+		"\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3"+
+		"\3\4\3\4\3\4\2\2\5\2\4\6\2\2\2X\2\b\3\2\2\2\4:\3\2\2\2\6R\3\2\2\2\b\t"+
+		"\7\17\2\2\t\r\7\22\2\2\n\f\7\23\2\2\13\n\3\2\2\2\f\17\3\2\2\2\r\13\3\2"+
+		"\2\2\r\16\3\2\2\2\16\67\3\2\2\2\17\r\3\2\2\2\20\26\7\3\2\2\21\22\7\4\2"+
+		"\2\22\23\7\5\2\2\23\25\7\26\2\2\24\21\3\2\2\2\25\30\3\2\2\2\26\24\3\2"+
+		"\2\2\26\27\3\2\2\2\27\37\3\2\2\2\30\26\3\2\2\2\31\32\7\6\2\2\32\33\7\7"+
+		"\2\2\33\34\7\5\2\2\34\36\7\27\2\2\35\31\3\2\2\2\36!\3\2\2\2\37\35\3\2"+
+		"\2\2\37 \3\2\2\2 (\3\2\2\2!\37\3\2\2\2\"#\7\6\2\2#$\7\b\2\2$%\7\5\2\2"+
+		"%\'\7\30\2\2&\"\3\2\2\2\'*\3\2\2\2(&\3\2\2\2()\3\2\2\2)\61\3\2\2\2*(\3"+
+		"\2\2\2+,\7\6\2\2,-\7\t\2\2-.\7\5\2\2.\60\7\31\2\2/+\3\2\2\2\60\63\3\2"+
+		"\2\2\61/\3\2\2\2\61\62\3\2\2\2\62\64\3\2\2\2\63\61\3\2\2\2\64\66\7\n\2"+
+		"\2\65\20\3\2\2\2\669\3\2\2\2\67\65\3\2\2\2\678\3\2\2\28\3\3\2\2\29\67"+
+		"\3\2\2\2:<\7\20\2\2;=\7\23\2\2<;\3\2\2\2=>\3\2\2\2><\3\2\2\2>?\3\2\2\2"+
+		"?@\3\2\2\2@A\7\3\2\2AB\7\13\2\2BC\7\5\2\2CD\7\24\2\2DE\7\6\2\2EF\7\f\2"+
+		"\2FG\7\5\2\2GH\7\25\2\2HI\7\6\2\2IJ\7\r\2\2JK\7\5\2\2KL\7\32\2\2LM\7\6"+
+		"\2\2MN\7\16\2\2NO\7\5\2\2OP\7\33\2\2PQ\7\n\2\2Q\5\3\2\2\2RS\7\21\2\2S"+
+		"\7\3\2\2\2\t\r\26\37(\61\67>";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
